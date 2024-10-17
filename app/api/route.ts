f12807f8db7794ebb8147b80e7bdb79ca26cbac2
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-10-18 03:15:17 +0530

    checkpoint for post test in user

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,8 +1,65 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth"; // Make sure this path matches your setup
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+import prisma from "@/lib/db";
 
-export const GET = async () => {
-  const session = await getServerSession(authOptions);
-  return NextResponse.json({ session });
+export const GET = async (req: NextRequest) => {
+  const session = await getServerSession();
+  try {
+    if (session) {
+      const { name, email, image } = session.user;
+      const user = await prisma.user.create({
+        data: { name: name, email: email, image: image },
+      });
+      console.log(name, email, image);
+      return NextResponse.json(user, { status: 200 });
+    } else {
+      return NextResponse.json("Unauthorized", { status: 401 });
+    }
+  } catch (e: any) {
+    return new NextResponse("Error creating user: " + e.message, {
+      status: 500,
+    });
+  }
+};
+//
+// export const POST = async (req: NextRequest) => {
+//   const session = await getServerSession();
+//   try {
+//     if (session) {
+//       const { name, email, image } = session.user || {};
+//       const user = await prisma.user.create({
+//         data: { name: name, email: email, image: image },
+//       });
+//       console.log(name, email, image);
+//       return NextResponse.json(user, { status: 200 });
+//     } else {
+//       return NextResponse.json("Unauthorized", { status: 401 });
+//     }
+//   } catch (e: any) {
+//     return new NextResponse("Error creating user: " + e.message, {
+//       status: 500,
+//     });
+//   }
+// };
+export const POST = async (req: NextRequest) => {
+  const session = await getServerSession();
+
+  try {
+    const {
+      name = "Test User",
+      email = "test@example.com",
+      image = "default.jpg",
+    } = session?.user || {};
+
+    const user = await prisma.user.create({
+      data: { name: name, email: email, image: image },
+    });
+
+    console.log(name, email, image);
+    return NextResponse.json(user, { status: 200 });
+  } catch (e: any) {
+    return new NextResponse("Error creating user: " + e.message, {
+      status: 500,
+    });
+  }
 };
