ccbfb46f6fa93bdaeb9d21b24ced59b503ca11ed
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-10-18 03:36:00 +0530

    made get router data

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,44 +3,16 @@ import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/lib/db";
 
 export const GET = async (req: NextRequest) => {
-  const session = await getServerSession();
   try {
-    if (session) {
-      const { name, email, image } = session.user;
-      const user = await prisma.user.create({
-        data: { name: name, email: email, image: image },
-      });
-      console.log(name, email, image);
-      return NextResponse.json(user, { status: 200 });
-    } else {
-      return NextResponse.json("Unauthorized", { status: 401 });
-    }
-  } catch (e: any) {
-    return new NextResponse("Error creating user: " + e.message, {
+    const users = await prisma.user.findMany();
+    return NextResponse.json({ success: true, data: users }, { status: 200 });
+  } catch (error: any) {
+    return new NextResponse("Error fetching users: " + error.message, {
       status: 500,
     });
   }
 };
-//
-// export const POST = async (req: NextRequest) => {
-//   const session = await getServerSession();
-//   try {
-//     if (session) {
-//       const { name, email, image } = session.user || {};
-//       const user = await prisma.user.create({
-//         data: { name: name, email: email, image: image },
-//       });
-//       console.log(name, email, image);
-//       return NextResponse.json(user, { status: 200 });
-//     } else {
-//       return NextResponse.json("Unauthorized", { status: 401 });
-//     }
-//   } catch (e: any) {
-//     return new NextResponse("Error creating user: " + e.message, {
-//       status: 500,
-//     });
-//   }
-// };
+
 export const POST = async (req: NextRequest) => {
   const session = await getServerSession();
 
