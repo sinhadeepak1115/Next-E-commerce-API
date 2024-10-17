f9dccd5748bc195041bbb0fcf56b923a9ee9f768
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-10-17 18:06:34 +0530

    added prisma and connected to db

--- app/api/hello.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "GET") {
-    res.status(200).json({ message: "Hello, world!" });
-  } else {
-    res.status(405).json({ message: "Method not allowed" });
-  }
-}
