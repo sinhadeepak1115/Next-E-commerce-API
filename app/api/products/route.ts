0838a3a1fa794fdf6ae9797ded7dd970dca9cffc
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-10-29 01:41:46 +0530

    complete delete route of product.

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,12 +1,13 @@
 import { getServerSession } from "next-auth";
 import { NextResponse, NextRequest } from "next/server";
-import prima from "@/lib/db";
+import prisma from "@/lib/db";
+import { error } from "console";
 
 export const GET = async (req: NextRequest) => {
-  const session = getServerSession();
+  const session = await getServerSession();
   try {
     if (session) {
-      const products = await prima.product.findMany();
+      const products = await prisma.product.findMany();
       return NextResponse.json(products, { status: 200 });
     } else {
       return NextResponse.json("Unauthorized", { status: 401 });
@@ -19,11 +20,9 @@ export const GET = async (req: NextRequest) => {
 };
 
 export const POST = async (req: NextRequest) => {
-  // const session = await getServerSession();
   try {
-    // if (session) {
     const { name, description, price, stock } = await req.json();
-    const product = await prima.product.create({
+    const product = await prisma.product.create({
       data: {
         name: name,
         description: description,
@@ -32,9 +31,56 @@ export const POST = async (req: NextRequest) => {
       },
     });
     return NextResponse.json(product, { status: 200 });
-    // } else {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    // }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 },
+    );
+  }
+};
+
+export const PATCH = async (req: NextRequest) => {
+  try {
+    const { id, name, description, price, stock } = await req.json();
+    if (!id) {
+      return NextResponse.json(
+        { error: "Product Id is Required" },
+        { status: 400 },
+      );
+    }
+    const product = await prisma.product.update({
+      where: { id },
+      data: {
+        name: name,
+        description: description,
+        price: price,
+        stock: stock,
+      },
+    });
+    return NextResponse.json(product, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 },
+    );
+  }
+};
+
+export const DELETE = async (req: NextRequest) => {
+  try {
+    const { id } = await req.json(); // Assuming req.json() returns an object with { id }
+    if (!id) {
+      return NextResponse.json(
+        { error: "Product Id is required for deletion" },
+        { status: 400 },
+      );
+    }
+    const product = await prisma.product.delete({
+      where: { id: id },
+    });
+    return NextResponse.json(product, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
